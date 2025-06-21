a426bf101086716890a49eb3de290689f921dbe6
NhatTranANHP <nhattrananhp@gmail.com>
2025-06-21 11:10:24 +0700

    chang a to Link in guide.tsx

--- pages/pages/guide.tsx
+++ pages/pages/guide.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import { useTranslation } from "@/hook/useTranslation";
+import Link from "next/link";
 
 export const metadata = {
   title: "Fucoidan là gì?",
@@ -19,69 +20,69 @@ export default function GuidePage() {
     <h2>{t("ガイド一覧")}</h2>
     <dl>
         <dd>
-            <a href="/policies/shipping-policy">
+            <Link href="/policies/shipping-policy">
                 <span className="guide-cate">PAYMENT</span>
                 <p>{t("お支払いについて")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/policies/shipping-policy#deli-01">
+            <Link href="/policies/shipping-policy#deli-01">
                 <span className="guide-cate">DELIVERY</span>
                 <p>{t("配送について")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/policies/refund-policy">
+            <Link href="/policies/refund-policy">
                 <span className="guide-cate">RETURNS/EXHANGES</span>
                 <p>{t("キャンセル・返品・交換について")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/pages/guide-01">
+            <Link href="/pages/guide-01">
                 <span className="guide-cate">MAIL</span>
                 <p>{t("当店からのメールが届かないお客様へ")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/pages/guide-02">
+            <Link href="/pages/guide-02">
                 <span className="guide-cate">SUBSCRIPTION</span>
                 <p>{t("定期購入について")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/pages/guide-03">
+            <Link href="/pages/guide-03">
                 <span className="guide-cate">COUPON</span>
                 <p>{t("クーポンについて")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/pages/guide-04">
+            <Link href="/pages/guide-04">
                 <span className="guide-cate">ACCOUNT</span>
                 <p>{t("アカウントについて")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/policies/terms-of-service">
+            <Link href="/policies/terms-of-service">
                 <span className="guide-cate">POLICY</span>
                 <p>{t("ご利用規約")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/policies/privacy-policy">
+            <Link href="/policies/privacy-policy">
                 <span className="guide-cate">POLICY</span>
                 <p>{t("プライバシーポリシー")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/policies/legal-notice">
+            <Link href="/policies/legal-notice">
                 <span className="guide-cate">LEGAL NOTICE</span>
                 <p>{t("特定商取引法")}</p>
-            </a>
+            </Link>
         </dd>
         <dd>
-            <a href="/pages/about"> <span className="guide-cate">ABOUT US</span>
+            <Link href="/pages/about"> <span className="guide-cate">ABOUT US</span>
                 <p>{t("会社概要")}</p>
-            </a>
+            </Link>
         </dd>
     </dl>
 </div>
